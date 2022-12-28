29d6862a61385b91bfad56b39810650b59be2212
Anshul patle <patleanshul920@gmail.com>
2022-12-28 17:12:07 +0530

    update minor changes

--- frontend/src/component/RoomCard/RoomCard.jsx
+++ frontend/src/component/RoomCard/RoomCard.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import styles from "./RoomCard.module.css";
+
+
 const RoomCard = ({ room }) => {
   return (
     <div className={styles.card}>
       <h3 className={styles.topic}>{room.topic}</h3>
       <div className={styles.speakers}>
-      <div className={styles.avatars}>
-         {  room.speakers.map((speaker) => (
+        <div className={styles.avatars}>
+          {room.speakers.map((speaker) => (
             <img key={speaker.id} src={speaker.avatar} alt="speaker-avatar" />
-        ))}
-
-      </div>
-      <div className={styles.names}>
-      {room.speakers.map((speaker) => (
-        <div  key={speaker.id} className={styles.nameWrapper}>
-            <span>{speaker.name}</span>
-            <img src="/imags/chat-bubble.png" alt="chat-bubble" />
+          ))}
         </div>
-        ))}
-
-      </div>
-
+        <div className={styles.names}>
+          {room.speakers.map((speaker) => (
+            <div key={speaker.id} className={styles.nameWrapper}>
+              <span>{speaker.name}</span>
+              <img src="/imags/chat-bubble.png" alt="chat-bubble" />
+            </div>
+          ))}
+        </div>
       </div>
       <div className={styles.peopleCount}>
         <span>{room.totalPeople}</span>
