3d47ccfa51521a7e4294f5e9fde6a03eb3b38ada
Anshul patle <patleanshul920@gmail.com>
2023-01-02 23:59:13 +0530

    added new changes

---
+++ frontend/src/hooks/useStateWithCallback.js
@@ -0,0 +1,13 @@
+export const useStateWithCallback = () => {
+    
+    // useState([
+    //     {
+    //       id: 1,
+    //       name: 'Anshul',
+    //     },
+    //     {
+    //       id:2,
+    //       name: 'john doe',
+    //     },
+    //   ]);
+}
--- frontend/src/hooks/useWebRTC.js
+++ frontend/src/hooks/useWebRTC.js
@@ -1,15 +1,7 @@
 import { useState } from "react";
+import { useStateWithCallback } from "./useStateWithCallback";
 
 export const useWebRTC  = () => {
-    const [clients, setClients] = useState([
-        {
-          id: 1,
-          name: 'Anshul',
-        },
-        {
-          id:2,
-          name: 'john doe',
-        },
-      ]);
+    const [clients, setClients] = useStateWithCallback()
      return { clients };
 };
