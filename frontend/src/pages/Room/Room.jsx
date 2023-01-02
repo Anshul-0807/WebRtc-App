cbabaa385cf587346471d02574fb0849143258bc
Anshul patle <patleanshul920@gmail.com>
2023-01-02 23:37:00 +0530

    added new changes

--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Room = () => {
+
+const [clients, setClients] = useState([
+  {
+    id: 1,
+    name: 'Anshul',
+  },
+  {
+    id:2,
+    name: 'john doe',
+  },
+])
+
   return (
-    <div>This is our single room jkhslllllllllllll
-    dddddddddddddddddddddd
-    gggggggggggggggggggggg
-    hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</div>
+    <div>
+    <h1>All connected clients</h1>
+    {
+      clients.map(client =>{
+        return <div>
+          <audio controls autoPlay></audio>
+        </div>
+      })
+    }
+    </div>
   )
 }
 
