15a8770b92930c5ca2a834139d74632b7e78b712
Anshul patle <patleanshul920@gmail.com>
2023-01-03 16:04:44 +0530

    Update useWebRTC.js

--- frontend/src/hooks/useWebRTC.js
+++ frontend/src/hooks/useWebRTC.js
@@ -1,44 +1,49 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useCallback, useEffect } from "react";
 import { useStateWithCallback } from "./useStateWithCallback";
 
-const users = [
-    {
-        id: 1,
-        name: 'Anshul',
-      },
-      {
-        id:2,
-        name: 'john doe',
-      },
-]
 
-export const useWebRTC  = (roomId, user) => {
-    const [clients, setClients] = useStateWithCallback(users);
-    const audioElements = useRef({});
-    const connections = useRef({});
-    const localMediaStream = useRef(null); 
+export const useWebRTC = (roomId, user) => {
+  const [clients, setClients] = useStateWithCallback([]);
+  const audioElements = useRef({});
+  const connections = useRef({});
+  const localMediaStream = useRef(null);
 
+ 
 
-    const provideRef = (instance, userId) => {
-        audioElements.current[userId] = instance;
-    }
+  const addNewClients = useCallback(
+    (newClient, cb) => {
+      const lookingFor = clients.find((client) => client.id === newClient.id);
 
-    const addNewClients = 
+      if (lookingFor === undefined) {
+        setClients((existingClient) => [...existingClient, newClient], cb);
+      }
+    },
+    [clients, setClients]
+  );
 
-    //   capture media
+  //   capture media
 
-     useEffect(() => {
-       const startCapture = async () => {
-        localMediaStream.current = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-        });
-       };
+  useEffect(() => {
+    const startCapture = async () => {
+      localMediaStream.current = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+    };
 
-       startCapture().then(() => {
+    startCapture().then(() => {
+        addNewClients(user, () => {
+            const localElement = audioElements.current[user.id];
+            if(localElement){
+                localElement.volume = 0;
+                localElement.srcObject = localMediaStream.current;
+            }
+        })
+    });
+  }, []);
 
-       })
-     }, [])
-     
+  const provideRef = (instance, userId) => {
+    audioElements.current[userId] = instance;
+  };
 
-     return { clients, provideRef };
+  return { clients, provideRef };
 };
