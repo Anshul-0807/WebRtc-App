04cf92de436f4bf198a556be009001c4670c1e81
Anshul patle <patleanshul920@gmail.com>
2023-01-03 14:22:57 +0530

    added new changes

--- frontend/src/hooks/useStateWithCallback.js
+++ frontend/src/hooks/useStateWithCallback.js
@@ -1,13 +1,24 @@
-export const useStateWithCallback = () => {
-    
-    // useState([
-    //     {
-    //       id: 1,
-    //       name: 'Anshul',
-    //     },
-    //     {
-    //       id:2,
-    //       name: 'john doe',
-    //     },
-    //   ]);
-}
+import { useCallback, useRef, useState, useEffect } from "react";
+
+export const useStateWithCallback = (initialState) => {
+  const [state, setState] = useState(initialState);
+  const cbRef = useRef();
+
+  const updateState = useCallback((newState, cb) => {
+    cbRef.current = cb;
+
+    setState((prev) => {
+      return typeof newState === "function" ? newState(prev) : newState;
+    });
+  }, []);
+
+  useEffect(() => {
+    if(cbRef.current){
+        cbRef.current(state);
+        cbRef.current = null;
+    }
+  
+  }, [state])
+  
+  return [state, updateState];
+};
--- frontend/src/hooks/useWebRTC.js
+++ frontend/src/hooks/useWebRTC.js
@@ -2,6 +2,16 @@ import { useState } from "react";
 import { useStateWithCallback } from "./useStateWithCallback";
 
 export const useWebRTC  = () => {
-    const [clients, setClients] = useStateWithCallback()
+    const [clients, setClients] = useStateWithCallback([
+        {
+            id: 1,
+            name: 'Anshul',
+          },
+          {
+            id:2,
+            name: 'john doe',
+          },
+    ]);
+
      return { clients };
 };
