bb7d8875c61708a4d932dd2d7df44ed87d623cab
Anshul patle <patleanshul920@gmail.com>
2023-01-05 23:59:10 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,82 +1,78 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const router = require('./routes')
+const router = require("./routes");
 
-const DbConnect = require('./database');
+const DbConnect = require("./database");
 
-const cors = require('cors');
+const cors = require("cors");
 
-const cookieparser = require('cookie-parser');
+const cookieparser = require("cookie-parser");
 
-const ACTIONS = require('./actions');
+const ACTIONS = require("./actions");
 
-const server = require('http').createServer(app);
+const server = require("http").createServer(app);
 
-const io = require('socket.io')(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST'],
-    },
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 app.use(cookieparser());
 
 const corsOption = {
+  credentials: true,
 
-    credentials: true,
+  origin: ["http://localhost:3000"],
+};
+app.use(cors(corsOption));
 
-    origin : ['http://localhost:3000'],
-}
-app.use(cors(corsOption))
-
-app.use('/storage', express.static('storage'));
+app.use("/storage", express.static("storage"));
 
 const PORT = process.env.PORT || 5500;
 
 DbConnect();
 
-app.use(express.json({limit: '8mb'}));
+app.use(express.json({ limit: "8mb" }));
 
 app.use(router);
 
-app.get('/', (req,res) => {
-
-    res.send('hello from express js')
+app.get("/", (req, res) => {
+  res.send("hello from express js");
 });
 
 // sockets
 
-const socketUserMapping = {
+const socketUserMapping = {};
 
-}
+io.on("connection", (socket) => {
+  console.log("new connection ", socket.id);
 
-io.on('connection', (socket) => {
-    console.log('new connection ', socket.id);
+  socket.on(ACTIONS.JOIN, ({ roomId, user }) => {
+    socketUserMapping[socket.id] = user;
 
-    socket.on(ACTIONS.JOIN, ({roomId, user}) => {
-        socketUserMapping[socket.id] = user;
-
-         //new map
+    //new map
     const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
-    clients.forEach(clientId => {
-        io.to(clientId).emit(ACTIONS.ADD_PEER, {
-          peerId: socket.id,
-          createOffer: false,
-          user,
-        } );
+    clients.forEach((clientId) => {
+      io.to(clientId).emit(ACTIONS.ADD_PEER, {
+        peerId: socket.id,
+        createOffer: false,
+        user,
+      });
     });
     socket.emit(ACTIONS.ADD_PEER, {
-        peerId: clientId,
-        createOffer: true,
-        user: socketUserMapping[clientId],
+      peerId: clientId,
+      createOffer: true,
+      user: socketUserMapping[clientId],
     });
 
     socket.join(roomId);
-    }); 
+  });
 });
 
 server.listen(PORT, () => console.log(` listening on port ${PORT}`));
