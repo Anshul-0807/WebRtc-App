3a146c15d8abf3bd562f141da1a749d61d0e6933
Anshul patle <patleanshul920@gmail.com>
2023-01-12 17:24:05 +0530

    Update Room.jsx

--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -1,70 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useWebRTC } from "../../hooks/useWebRTC";
 import { useParams, useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
-import styles from './Room.module.css';
+import styles from "./Room.module.css";
+import { getRoom } from "../../http";
 
 const Room = () => {
   const { id: roomId } = useParams();
   const user = useSelector((state) => state.auth.user);
   const { clients, provideRef } = useWebRTC(roomId, user);
   const history = useHistory();
+  const [room, setRoom] = useState(null);
 
-  const handleManualLeave = () =>{
-   history.push('/rooms');
+  const handleManualLeave = () => {
+    history.push("/rooms");
   };
 
-  
+  useEffect(() => {
+    const fetchRoom = async () => {
+      const { data } = await getRoom(roomId);
+      setRoom((prev) => data);
+    };
+
+    fetchRoom();
+  }, [roomId]);
 
   return (
     <div>
-    <div className="container">
-         <button onClick={handleManualLeave} className={styles.goBack}>
-         <img src="/imags/goback.png" alt="goback" />
-         <span>All voice rooms</span>
-         </button>
-    </div>
-    <div className={styles.clientsWrap}> 
-    <div className={styles.header}>
-      <h2 className={styles.topic}>Node js is Awesome</h2>
-      <div className={styles.actions}>
-        <button className={styles.actionBtn}>
-          <img src="/imags/palm5.png" alt="palm-icon" />
-
-        </button>
-        <button onClick={handleManualLeave} className={styles.actionBtn}>
-          <img src="/imags/piece.png" alt="piece-emoji" />
-          <span> Leave quietly </span>
+      <div className="container">
+        <button onClick={handleManualLeave} className={styles.goBack}>
+          <img src="/imags/goback.png" alt="goback" />
+          <span>All voice rooms</span>
         </button>
       </div>
-    </div>
-      <div className={styles.clientsList}>
-      {clients.map((client) => {
-        return (
-          <div className={styles.client}>
-          <div className={styles.userHead} key={client.id}>
-            <audio
-              ref={(instance) => provideRef(instance, client.id)}
-              // controls
-              autoPlay
-            ></audio>
-            <img className={styles.userAvatar} src={client.avatar} alt="avatar" />
-           <button className={styles.micBtn}>
-            {/* <img 
+      <div className={styles.clientsWrap}>
+        <div className={styles.header}>
+          <h2 className={styles.topic}>{room?.topic}</h2>
+          <div className={styles.actions}>
+            <button className={styles.actionBtn}>
+              <img src="/imags/palm5.png" alt="palm-icon" />
+            </button>
+            <button onClick={handleManualLeave} className={styles.actionBtn}>
+              <img src="/imags/piece.png" alt="piece-emoji" />
+              <span> Leave quietly </span>
+            </button>
+          </div>
+        </div>
+        <div className={styles.clientsList}>
+          {clients.map((client) => {
+            return (
+              <div className={styles.client} key={client.id} >
+                <div className={styles.userHead} >
+                  <audio
+                    ref={(instance) => provideRef(instance, client.id)}
+                    // controls
+                    autoPlay
+                  ></audio>
+                  <img
+                    className={styles.userAvatar}
+                    src={client.avatar}
+                    alt="avatar"
+                  />
+                  <button className={styles.micBtn}>
+                    {/* <img 
             src="/imags/"
              alt="mic-icon" /> */}
-            
-           <img
-            src="/imags/mic-mute2.png" 
-            alt="mic-mute" />
 
-           </button>
-          </div>
-          <h4>{client.name}</h4>
-          </div>
-        );
-      })}
-      </div>
+                    <img src="/imags/mic-mute2.png" alt="mic-mute" />
+                  </button>
+                </div>
+                <h4>{client.name}</h4>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
