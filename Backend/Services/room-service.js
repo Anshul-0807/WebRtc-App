1b495622fd00e3453c783f09f2c18999f057a086
Anshul patle <patleanshul920@gmail.com>
2023-01-29 15:26:43 +0530

    Update room-service.js

--- Backend/Services/room-service.js
+++ Backend/Services/room-service.js
@@ -15,17 +15,16 @@ class RoomService {
 
   async getAllRooms(types) {
     const rooms = await RoomModel.find({ roomType: { $in: types } })
-      .populate({ path: 'speakers', select: 'avatar' })
-      .populate({ path: 'ownerId', select: '_id' })
+      .populate({ path: "speakers", select: "avatar" })
+      .populate({ path: "ownerId", select: "_id" })
       .exec();
-      console.log(rooms[0].speakers);
+    console.log(rooms[0].speakers);
     return rooms;
   }
 
-   async getRoom(roomId) {
-      const room = await RoomModel.findOne({ _id: roomId });
-      return room;
-   }
-
+  async getRoom(roomId) {
+    const room = await RoomModel.findOne({ _id: roomId });
+    return room;
+  }
 }
 module.exports = new RoomService();
