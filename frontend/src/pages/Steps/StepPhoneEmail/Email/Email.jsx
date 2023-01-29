f8d7b906d63a8c11db8b7f841b0b600bd21ef3ea
Anshul patle <patleanshul920@gmail.com>
2023-01-29 15:29:32 +0530

    Update Email.jsx

--- frontend/src/pages/Steps/StepPhoneEmail/Email/Email.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/Email/Email.jsx
@@ -1,35 +1,26 @@
-import React , {useState}from 'react';
-import Card from '../../../../component/shared/Card/Card';
-import Button from '../../../../component/shared/Button/Button';
-import TextInput from '../../../../component/shared/TextInput/TextInput';
+import React, { useState } from "react";
+import Card from "../../../../component/shared/Card/Card";
+import Button from "../../../../component/shared/Button/Button";
+import TextInput from "../../../../component/shared/TextInput/TextInput";
 import styles from "../StepPhoneEmail.module.css";
 
-
-export const Email = ({onNext}) => {
-
+export const Email = ({ onNext }) => {
   const [email, setemail] = useState("");
-  
-  return (
-    <Card
-    title="Enter your email id" icon="mailing" >
-     <TextInput
-        value={email}
-        onChange={(e) => setemail(e.target.value)}
-      />
 
-    
-          <div>
-          <div className={styles.actionButtonWrap}>
-          <Button text="Next" onClick={onNext}/>
+  return (
+    <Card title="Enter your email id" icon="mailing">
+      <TextInput value={email} onChange={(e) => setemail(e.target.value)} />
+
+      <div>
+        <div className={styles.actionButtonWrap}>
+          <Button text="Next" onClick={onNext} />
         </div>
         <p className={styles.BottomParagraph}>
           By entering your number. you're agreeing to our terms of service and
           private policy. Thanks.
         </p>
-          </div>
-         
+      </div>
     </Card>
-
-  )
-}
+  );
+};
 export default Email;
