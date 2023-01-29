790b2b0987c2375be06038d84940b9689d58da2f
Anshul patle <patleanshul920@gmail.com>
2023-01-29 15:25:42 +0530

    Update Auth-controller.js

--- Backend/Controller/Auth-controller.js
+++ Backend/Controller/Auth-controller.js
@@ -3,12 +3,12 @@ const hashservice = require("../Services/hash-service");
 const { sendBySms } = require("../Services/otp-service");
 const userService = require("../services/user-service");
 const tokenService = require("../Services/token-service");
-const UserDto = require('../dtos/user-dto')
+const UserDto = require("../dtos/user-dto");
 
 class Authcontroller {
   async sendOtp(req, res) {
     //logic
-      
+
     const { phone } = req.body;
     if (!phone) {
       res.status(400).json({ message: "Phone field is required" });
@@ -31,7 +31,7 @@ class Authcontroller {
       res.json({
         hash: `${hash}.${expires}`,
         phone,
-        otp,  //kaam hone ke baaad change karna...ye otp hatana and sendbysms ko uncomment karna
+        otp, //kaam hone ke baaad change karna...ye otp hatana and sendbysms ko uncomment karna
       });
     } catch (err) {
       console.log(err);
@@ -40,14 +40,14 @@ class Authcontroller {
     // res.json({hash : hash});
   }
 
-    async verifyOtp(req, res) {
+  async verifyOtp(req, res) {
     //logic
     const { otp, hash, phone } = req.body;
     if (!otp || !hash || !phone) {
       res.status(400).json({ message: "All fields are required! " });
     }
 
-    const [hashedOtp, expires] = hash.split('.');
+    const [hashedOtp, expires] = hash.split(".");
     if (Date.now() > +expires) {
       res.status(400).json({ message: "OTP expired!" });
     }
@@ -70,7 +70,7 @@ class Authcontroller {
       }
     } catch (err) {
       console.log(err);
-      res.status(500).json({ message: 'Db error' });
+      res.status(500).json({ message: "Db error" });
     }
 
     //token
@@ -80,96 +80,88 @@ class Authcontroller {
       activated: false,
     });
 
-   await  tokenService.storeRefreshToken(refreshToken, user._id );
+    await tokenService.storeRefreshToken(refreshToken, user._id);
 
-    res.cookie('refreshToken', refreshToken, {
+    res.cookie("refreshToken", refreshToken, {
       maxAge: 1000 * 60 * 60 * 24 * 30,
       httpOnly: true,
     });
 
-    res.cookie('accessToken', accessToken, {
+    res.cookie("accessToken", accessToken, {
       maxAge: 1000 * 60 * 60 * 24 * 30,
       httpOnly: true,
     });
 
-  const userDto = new UserDto(user);
+    const userDto = new UserDto(user);
 
-  res.json({ user: userDto, auth: true });
+    res.json({ user: userDto, auth: true });
   }
 
-  async refresh( req, res){
+  async refresh(req, res) {
     // get refresh token from cookie
     const { refreshToken: refreshTokenFromCookie } = req.cookies;
     // check if token is valid
     let userData;
-    try{
+    try {
       userData = await tokenService.verifyRefreshToken(refreshTokenFromCookie);
-    }catch(err) {
-      return res.status(401).json({message: 'Invalid Token'})
+    } catch (err) {
+      return res.status(401).json({ message: "Invalid Token" });
     }
     // check if token is valid
-    try{
-    const token = await tokenService.findRefreshToken(
-      userData._id,
-      refreshTokenFromCookie
-    );
-    
-    if(!token){
-      return res.status(401).json({message: 'Invalid Token'});
- 
-    }
-     
-    } catch (err){
-     return res.status(500).json({message: 'Internal error'});
+    try {
+      const token = await tokenService.findRefreshToken(
+        userData._id,
+        refreshTokenFromCookie
+      );
+
+      if (!token) {
+        return res.status(401).json({ message: "Invalid Token" });
+      }
+    } catch (err) {
+      return res.status(500).json({ message: "Internal error" });
     }
 
     // check if valid user
-    const user = await userService.findUser({_id: userData._id});
-    if(!user){
-      return res.status(404).json({message: 'No user'});
-
+    const user = await userService.findUser({ _id: userData._id });
+    if (!user) {
+      return res.status(404).json({ message: "No user" });
     }
     // generate new tokens
-      const { refreshToken, accessToken } = tokenService.generateTokens({
-        _id: userData._id,
-      });
-
-      // update refresh token
-       try{
-         await tokenService.updateRefreshToken(userData._id, refreshToken);
-       } catch (err){
-         return res.status(500).json({message: 'Internal error'});
-
-       }
-    // put in cookie
-    res.cookie('refreshToken', refreshToken, {
-      maxAge: 1000 * 60 * 60 * 24 * 30,
-      httpOnly: true,
+    const { refreshToken, accessToken } = tokenService.generateTokens({
+      _id: userData._id,
     });
 
-    res.cookie('accessToken', accessToken, {
-      maxAge: 1000 * 60 * 60 * 24 * 30,
-      httpOnly: true,
-    });
-// responce
-  const userDto = new UserDto(user);
-
-  res.json({ user: userDto, auth: true });
-  }
-
-    async logout(req, res){
-      
-      const {refreshToken} = req.cookies;
-      // delete refresh token from cookie
-         await  tokenService.removeToken(refreshToken);
-      // delete cookies
-      res.clearCookie('refreshToken');
-      res.clearCookie('accessToken');
-      res.json({ user: null, auth: false})
+    // update refresh token
+    try {
+      await tokenService.updateRefreshToken(userData._id, refreshToken);
+    } catch (err) {
+      return res.status(500).json({ message: "Internal error" });
     }
+    // put in cookie
+    res.cookie("refreshToken", refreshToken, {
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      httpOnly: true,
+    });
 
+    res.cookie("accessToken", accessToken, {
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      httpOnly: true,
+    });
+    // responce
+    const userDto = new UserDto(user);
 
+    res.json({ user: userDto, auth: true });
   }
 
+  async logout(req, res) {
+    const { refreshToken } = req.cookies;
+    // delete refresh token from cookie
+    await tokenService.removeToken(refreshToken);
+    // delete cookies
+    res.clearCookie("refreshToken");
+    res.clearCookie("accessToken");
+    res.json({ user: null, auth: false });
+  }
+}
 
 module.exports = new Authcontroller();
