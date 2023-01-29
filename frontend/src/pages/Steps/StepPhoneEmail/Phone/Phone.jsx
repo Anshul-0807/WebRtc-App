2b630ce19ba74cfd23c398ff11cc6a708e8fdad8
Anshul patle <patleanshul920@gmail.com>
2023-01-29 15:29:37 +0530

    Update Phone.jsx

--- frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -4,21 +4,21 @@ import Button from "../../../../component/shared/Button/Button";
 import TextInput from "../../../../component/shared/TextInput/TextInput";
 import styles from "../StepPhoneEmail.module.css";
 import { sendOtp } from "../../../../http";
-import { useDispatch } from 'react-redux'; 
+import { useDispatch } from "react-redux";
 import { setOtp } from "../../../../store/authSlice";
 
-const Phone = ({onNext}) => {
+const Phone = ({ onNext }) => {
   const [phoneNumber, setphoneNumber] = useState("");
   const dispatch = useDispatch();
 
   async function submit() {
-     //send request 
-       if(!phoneNumber) return;
-      const {data} = await sendOtp({phone : phoneNumber });
-      console.log(data);
-      dispatch(setOtp({phone : data.phone ,hash: data.hash}));
-        onNext();
-       }
+    //send request
+    if (!phoneNumber) return;
+    const { data } = await sendOtp({ phone: phoneNumber });
+    console.log(data);
+    dispatch(setOtp({ phone: data.phone, hash: data.hash }));
+    onNext();
+  }
 
   return (
     <Card title="Enter your phone number" icon="Phone">
@@ -29,8 +29,8 @@ const Phone = ({onNext}) => {
 
       <div>
         <div className={styles.actionButtonWrap}>
-          <Button text="Next" onClick={submit }/>
-        </div> 
+          <Button text="Next" onClick={submit} />
+        </div>
         <p className={styles.BottomParagraph}>
           By entering your number. you're agreeing to our terms of service and
           private policy. Thanks.
