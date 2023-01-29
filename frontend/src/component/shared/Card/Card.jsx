d8a55859c6afd7c9cb818d4c118b9e2981c5bfb5
Anshul patle <patleanshul920@gmail.com>
2023-01-29 15:27:56 +0530

    Update Card.jsx

--- frontend/src/component/shared/Card/Card.jsx
+++ frontend/src/component/shared/Card/Card.jsx
@@ -5,8 +5,8 @@ export const Card = ({ title, icon, children }) => {
   return (
     <div className={styles.Card}>
       <div className={styles.headingWrapper}>
-       {icon && <img src={`/imags/${icon}.png`} alt="logo" />}
-       {title && <h1 className={styles.heading}>{title}</h1>}
+        {icon && <img src={`/imags/${icon}.png`} alt="logo" />}
+        {title && <h1 className={styles.heading}>{title}</h1>}
       </div>
       {children}
     </div>
