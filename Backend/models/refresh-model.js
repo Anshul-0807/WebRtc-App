8b5c429d36e30f8690bd1e1e970015df4bca58cf
Anshul patle <patleanshul920@gmail.com>
2023-01-29 15:26:12 +0530

    Update refresh-model.js

--- Backend/models/refresh-model.js
+++ Backend/models/refresh-model.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const refreshSchema = new Schema({
-
-    token : {type : String , required : true },
-    userId: {type: Schema.Types.ObjectId, Ref: 'User'},
-},
-{
-    timestamps : true,
-}
+const refreshSchema = new Schema(
+  {
+    token: { type: String, required: true },
+    userId: { type: Schema.Types.ObjectId, Ref: "User" },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model('Refresh' , refreshSchema, 'tokens' );
+module.exports = mongoose.model("Refresh", refreshSchema, "tokens");
