3128e00d88c728a5cfe1096694b816a7f4cd52ce
Anshul patle <patleanshul920@gmail.com>
2023-01-29 15:26:37 +0530

    Update otp-service.js

--- Backend/Services/otp-service.js
+++ Backend/Services/otp-service.js
@@ -1,43 +1,39 @@
-const crypto = require('crypto');
-const hashservice = require('./hash-service')
+const crypto = require("crypto");
+const hashservice = require("./hash-service");
 
-const smsSid = process.env.SMS_SID
-const smsAuthToken = process.env.SMS_AUTH_TOKEN
-const twilio = require('twilio')(smsSid, smsAuthToken, {
-  lazyLoading : true
-})
+const smsSid = process.env.SMS_SID;
+const smsAuthToken = process.env.SMS_AUTH_TOKEN;
+const twilio = require("twilio")(smsSid, smsAuthToken, {
+  lazyLoading: true,
+});
 
 class OtpService {
-   async generateOtp() {
+  async generateOtp() {
     const otp = crypto.randomInt(1000, 9999);
     return otp;
   }
 
-  async sendBySms(phone , otp) {
+  async sendBySms(phone, otp) {
     return await twilio.messages.create({
-      to : phone ,
-      from : process.env.SMS_FROM_NUMBER,
-      body : `Your web-rtc OTP is ${otp}`,
-    })
+      to: phone,
+      from: process.env.SMS_FROM_NUMBER,
+      body: `Your web-rtc OTP is ${otp}`,
+    });
   }
 
-  verifyOtp(hashedOtp , data) {
-
+  verifyOtp(hashedOtp, data) {
     let computerhash = hashservice.hashOtp(data);
-    
+
     return computerhash === hashedOtp;
 
-  //   if(computerhash === hashedOtp){
+    //   if(computerhash === hashedOtp){
 
-  //     return true;
-  //   }
+    //     return true;
+    //   }
 
-  //   return false;
-  //
-
-  
- }
-
+    //   return false;
+    //
+  }
 }
 
 module.exports = new OtpService();
