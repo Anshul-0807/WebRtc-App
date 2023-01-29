65ced17ad66362cf3daf833c4402513d8065b236
Anshul patle <patleanshul920@gmail.com>
2023-01-29 15:26:19 +0530

    Update room-model.js

--- Backend/models/room-model.js
+++ Backend/models/room-model.js
@@ -1,25 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const roomSchema = new Schema({
-
-    topic : {type : String , required : true },
-    roomType : {type : String , required : true },
-    ownerId: {type: Schema.Types.ObjectId, Ref: 'User'},
-    speakers : {
-        type: [
-            {
-                type: Schema.Types.ObjectId, 
-                Ref: 'User'
-            }
-        ],
-        required: false,
-    }
-},
-{
-    timestamps : true,
-}
+const roomSchema = new Schema(
+  {
+    topic: { type: String, required: true },
+    roomType: { type: String, required: true },
+    ownerId: { type: Schema.Types.ObjectId, Ref: "User" },
+    speakers: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          Ref: "User",
+        },
+      ],
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model('Room' , roomSchema, 'rooms' );
+module.exports = mongoose.model("Room", roomSchema, "rooms");
