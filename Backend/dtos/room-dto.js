bfc5e23b4407f13b9bd354284aab5a67d43b99ed
Anshul patle <patleanshul920@gmail.com>
2023-01-29 15:25:56 +0530

    Update room-dto.js

--- Backend/dtos/room-dto.js
+++ Backend/dtos/room-dto.js
@@ -1,20 +1,19 @@
-class RoomDto{
-    id;
-    topic;
-    roomType;
-    speakers;
-    ownerId;
-    createdAt;
+class RoomDto {
+  id;
+  topic;
+  roomType;
+  speakers;
+  ownerId;
+  createdAt;
 
-
-    constructor(room) {
-        this.id = room._id;
-        this.topic = room.topic;
-        this.roomType = room.roomType;
-        this.ownerId = room.ownerId;
-        this.speakers = room.speakers;
-        this.createdAt = room.createdAt;
-    }
+  constructor(room) {
+    this.id = room._id;
+    this.topic = room.topic;
+    this.roomType = room.roomType;
+    this.ownerId = room.ownerId;
+    this.speakers = room.speakers;
+    this.createdAt = room.createdAt;
+  }
 }
 
 module.exports = RoomDto;
