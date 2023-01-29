ea70a8435a7fe2e7e928caddba6043f8e06bc417
Anshul patle <patleanshul920@gmail.com>
2023-01-29 15:26:05 +0530

    Update auth-middleware.js

--- Backend/middlewares/auth-middleware.js
+++ Backend/middlewares/auth-middleware.js
@@ -1,20 +1,18 @@
 const tokenService = require("../Services/token-service");
 
- module.exports = async function(req,res,next){
- try{
- const { accessToken } = req.cookies;
- if(!accessToken){
-    throw new Error();
- }
- const userData = await tokenService.verifyAccessToken(accessToken);
- if(!userData){
-    throw new Error();
- }
- req.user = userData;
- next();
- } catch(err){
- res.status(401).json({ message: 'Invalid token'});
-
- }
-   
-}; 
+module.exports = async function (req, res, next) {
+  try {
+    const { accessToken } = req.cookies;
+    if (!accessToken) {
+      throw new Error();
+    }
+    const userData = await tokenService.verifyAccessToken(accessToken);
+    if (!userData) {
+      throw new Error();
+    }
+    req.user = userData;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
