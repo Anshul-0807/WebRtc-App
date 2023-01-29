0f54616732905d787d538b1eed6bf3117dae35b3
Anshul patle <patleanshul920@gmail.com>
2023-01-29 15:29:03 +0530

    Update Activate.jsx

--- frontend/src/pages/Activate/Activate.jsx
+++ frontend/src/pages/Activate/Activate.jsx
@@ -1,35 +1,24 @@
-import React, {useState} from 'react';
-import StepName from '../Steps/StepName/StepName';
-import StepAvatar from '../Steps/StepAvatar/StepAvatar';
+import React, { useState } from "react";
+import StepName from "../Steps/StepName/StepName";
+import StepAvatar from "../Steps/StepAvatar/StepAvatar";
 
 const steps = {
-
   1: StepName,
-  2: StepAvatar
-}
+  2: StepAvatar,
+};
 
 export const Activate = () => {
-   const [step, setStep] = useState(1);
-   const Step = steps[step];
-   
-    function onNext(){
-      setStep(step + 1);
-    }
+  const [step, setStep] = useState(1);
+  const Step = steps[step];
+
+  function onNext() {
+    setStep(step + 1);
+  }
 
   return (
-    <div className='cardWrapper'>
-    <Step onNext = {onNext}></Step>
-   </div>
-  )
-}
-export default Activate; 
-
-
-
-
-
-
-
-
-
-
+    <div className="cardWrapper">
+      <Step onNext={onNext}></Step>
+    </div>
+  );
+};
+export default Activate;
