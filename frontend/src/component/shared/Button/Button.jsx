2cec816a26e1a13521f9a10523adaf476330c333
Anshul patle <patleanshul920@gmail.com>
2023-01-29 15:27:35 +0530

    Update Button.jsx

--- frontend/src/component/shared/Button/Button.jsx
+++ frontend/src/component/shared/Button/Button.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import styles from './Button.module.css'
-export const Button = ({text,onClick}) => {
+import React from "react";
+import styles from "./Button.module.css";
+export const Button = ({ text, onClick }) => {
   return (
     <div>
-         <button onClick={onClick} className={styles.Button}>
-             <span>{text}</span>
-              <img  className={styles.arrow} src="/imags/Arrow forward.png" alt="" />
-            </button>
+      <button onClick={onClick} className={styles.Button}>
+        <span>{text}</span>
+        <img className={styles.arrow} src="/imags/Arrow forward.png" alt="" />
+      </button>
     </div>
-  )
-}
+  );
+};
 export default Button;
